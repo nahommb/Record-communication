71a2ef6fb7b7bd3e2f1d3c15c94808ff335dec2c
nahom <nahomjr17@gmail.com>
2025-08-31 03:06:46 -0500

    update

--- client/src/components/services.jsx
+++ client/src/components/services.jsx
@@ -2,25 +2,42 @@ import ServicesItems from "./services_items";
 
 export default function Services(){
 
-    return <div className="mt-10 mb-3 p-4 bg-gradient-to-b from-orange to-blueAccent items-center">
-        <h2 className="text-white mb-4 sm:mt-12 font-bold text-lg">Our Services</h2>
-    <div className="bg-pink-500 flex w-full sm:justify-center overflow-x-auto space-x-4">
+    return <div className="mt-10 mb-3 p-4 sm:bg-gradient-to-b from-orange to-blueAccent bg-orange items-center">
+        <h2 className="text-white mb-4 sm:mt-12 font-bold text-lg sm:text-3xl">Our Services</h2>
+    <div>
+        <div className="flex justify-center ">
+            <h2 className="font-bold text-white sm:text-xl mb-4">Social Media Markating</h2>
+        </div>
+        <div className=" flex w-full sm:justify-around overflow-x-auto space-x-4">
         <ServicesItems />
         <ServicesItems />
         <ServicesItems />
     </div>
+        </div>
+
    <div className='my-4 h-1 bg-white'/>
-         <div className="bg-pink-500 flex w-full sm:justify-center overflow-x-auto space-x-4">
+       <div>
+        <div className="flex justify-center ">
+            <h2 className="font-bold text-white sm:text-xl mb-4">Social Media Markating</h2>
+        </div>
+        <div className=" flex w-full sm:justify-around overflow-x-auto space-x-4">
         <ServicesItems />
         <ServicesItems />
         <ServicesItems />
     </div>
+        </div>
+
     <div className='my-4 h-1 bg-white'/>
-     <div className="bg-pink-500 flex w-full sm:justify-center overflow-x-auto space-x-4">
+       <div>
+        <div className="flex justify-center ">
+            <h2 className="font-bold text-white sm:text-xl mb-4">Social Media Markating</h2>
+        </div>
+        <div className=" flex w-full sm:justify-around overflow-x-auto space-x-4">
         <ServicesItems />
         <ServicesItems />
         <ServicesItems />
     </div>
+        </div>
 
     </div>
 }
--- client/src/components/services_items.jsx
+++ client/src/components/services_items.jsx
@@ -2,13 +2,14 @@
 export default function ServicesItems(){
 
 
-    return  <div className="w-60 h-60 bg-red-500 items-center ">
-            <div className="bg-white h-20 w-20 rounded-full mx-auto">
-
-            </div>
-            <div className="w-60">
-                <h2 className="font-bold text-center mt-2">Neymar Junior</h2>
-                <p className="text-sm text-justify mt-1">brahoi ioiwuindoawjpo oiuw wiju8yh roidn foieunaowi oaiwu dojfalwkj</p>
-            </div>
+    return  <div className="w-60 h-60 sm:w-80 sm:h-80 flex flex-col items-center justify-center">
+        <div className="bg-white h-20 w-20 rounded-full"></div>
+        <div className="w-60 sm:w-80 p-2 text-center">
+            <h2 className="font-bold mt-2 text-white text-lg sm:text-xlg">Neymar Junior</h2>
+            <p className="text-sm text-justify mt-1 text-white">
+            brahoi ioiwuindoawjpo oiuw wiju8yh roidn lee bease  uwhduwa oiwhdi oiuwu  ywa dnufioiwdh oiwui oiayqpj dydhowih foieunaowi oaiwu dojfalwkj
+            </p>
         </div>
+        </div>
+
 }
